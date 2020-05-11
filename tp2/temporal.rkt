96fa1ef37da06f3d5223df7290e30505e3db14cc
victoria-peinado <victoria-peinado@u.com>
2020-05-11 15:37:09 -0300

    Update temporal.rkt

--- tp2/temporal.rkt
+++ tp2/temporal.rkt
@@ -3,20 +3,23 @@
 (require 2htdp/universe)
 (require test-engine/racket-tests)
 
+;El estado del big-bang serán números
+
 (define ALTO 500)
-(define ANCHO 1015)
-(define M-ALTO 250) ; mitad alto
+(define ANCHO 1020)
+(define M-ALTO 250); Mitad del alto
 (define ESCENA (empty-scene ANCHO ALTO))
+
 
 ; Función posicion-numero. Asigna a cada numero de 0 a 100 una posición
 ; posicion-numero Number -> Number
-(check-expect(posicion-numero 0) 0)
-(check-expect(posicion-numero 15) 150)
-(check-expect(posicion-numero 100) 1000)
+(check-expect(posicion-numero 0) 5)
+(check-expect(posicion-numero 15) 155)
+(check-expect(posicion-numero 100) 1005)
 
 (define
   (posicion-numero num)
-  (* num 10)
+  (+ (* num 10) 5)
   )
 
 ; Función color-numero. Asigna un color a cada numero del 0 al 100
@@ -29,6 +32,7 @@
   (if (= 0 (modulo num 2)) "blue" "red")
   )
 
+; Utilizamos números para representa el texto que se mostrara en pantalla
 ; Función numero. Agrega un numero a la escena. La posicion y color del numero depende del mismo
 ; numero: Number String -> image?
 (define
@@ -36,12 +40,17 @@
   (place-image (text (number->string num) 18 (color-numero num)) (posicion-numero num) M-ALTO ESCENA)
   )
 
+; Utilizamos números para representa el texto que se mostrara en pantalla
 ; Funcion dibujar. Por ahora solo muestra la escena vacía
-; dibujar: Number -> image? 
-(define (dibujar estado) (numero estado))
+; dibujar: Estado -> image? 
+(define
+  (dibujar estado)
+  (numero estado)
+  )
 
-; Función avanzar. Avanza el estado de 0 a 100 aleatoriamente.
-; Si se pasa un estado invalido se devuelve 50.
+; Utilizamos un número(101) para representar un intervalo([0,100]).
+; Función avanzar. Avanza el estado a un numero del itervalo [0,100] aleatoriamente.
+; Si es el estado inicial devuelve 50.
 ; avanzar-estado: Number -> Number
 (check-random (avanzar 0) (random 101))
 (check-expect (avanzar -1) 50)
@@ -51,7 +60,7 @@
   (if (= -1 estado) 50 (random 101))
   )
 
-; Función terminar?. Determina si el estado es multiplo de 8 que debe terminar
+; Función terminar?: Determina si el estado es multiplo de 8 que debe terminar
 ; terminar?: Number -> Boolean
 (check-expect(terminar? 10) #false)
 (check-expect(terminar? 8) #true)
@@ -62,12 +71,26 @@
   (if (= 0 (modulo estado 8)) #true #false)
   )
 
-(test)
+
+; Función manejar-teclado:si se presiona la flecha derecha el estado pasa a ser 100,
+; si se presiona la flecha izquierda el estado pasa a ser 0.
+
+; manejar-teclado: Number Key-event -> Number
+(check-expect(manejar-teclado 12 "left") 0)
+(check-expect(manejar-teclado 12 "right") 100)
+(define
+  (manejar-teclado estado key) (cond
+                                 [(key=? key "left") 0]
+                                 [(key=? key "right") 100]))
+
 
 (big-bang -1
-  [state #t]
+  ;[state #t]
   [on-tick avanzar 2]
   [on-draw dibujar]
-  [stop-when terminar?]
-  [close-on-stop #t]
+  [on-key manejar-teclado]
+  [stop-when terminar? dibujar]
+  [close-on-stop 3]
   )
+
+(test)
