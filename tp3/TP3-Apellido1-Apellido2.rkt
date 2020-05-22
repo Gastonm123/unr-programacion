5d150c334302fd3aecd8817ff85d5b413860de35
Gaston <gastonmartinezcasto@gmail.com>
2020-05-22 17:26:36 -0300

    Comentarios y tests

--- tp3/TP3-Apellido1-Apellido2.rkt
+++ tp3/TP3-Apellido1-Apellido2.rkt
@@ -7,8 +7,15 @@ Integrantes:
 - [Apellido, Nombre], comisión [número_comisión].
 |#
 
+(require test-engine/racket-tests)
+
 ;Ejercicio 1
 (struct Circunferencia (x y radio))
+
+; Circunferencias para pruebas
+(define C1 (Circunferencia 0 0 2.5))
+(define C2 (Circunferencia 3 4 2.5))
+(define C3 (Circunferencia 5 0 5))
 
 ;Ejercicio 2
 ;distancia: Circunferencia Cirdunfe
@@ -25,10 +32,16 @@ Integrantes:
      ))
     )))
 
+; tangentes-exteriores?: Define si dos circulos tienen un unico punto en comun
+; y no contiene una a la otra.
+; tangentes-exteriores?: Circunferencia Circunferencia -> Boolean
 (define (tangentes-exteriores? c c2)
   (=
    (distancia c c2)
    (+ (Circunferencia-radio c) (Circunferencia-radio c2))))
+
+(check-expect (tangentes-exteriores? C1 C2) #t)
+(check-expect (tangentes-exteriores? C1 C3) #f)
   
 ;Ejercicio 3
 (define (crear-tangente-exterior c n)
@@ -37,3 +50,5 @@ Integrantes:
    (Circunferencia-y c) ; y
    (* n (Circunferencia-radio c)) ; radio
   ))
+
+(test)
