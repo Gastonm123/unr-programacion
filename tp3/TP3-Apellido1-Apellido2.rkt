58e4ad3f84f147ed9f21ab17a82e67a53173fe06
victoria-peinado <victoria-peinado@u.com>
2020-05-22 15:56:04 -0300

    ejercicio 2

--- tp3/TP3-Apellido1-Apellido2.rkt
+++ tp3/TP3-Apellido1-Apellido2.rkt
@@ -11,7 +11,24 @@ Integrantes:
 (struct Circunferencia (x y radio))
 
 ;Ejercicio 2
-
-
+(define (distancia c c2)
+   (sqrt
+    (+
+     (sqr(-
+          (Circunferencia-x c)
+          (Circunferencia-x c2)
+      ))
+     (sqr(-
+          (Circunferencia-y c )
+          (Circunferencia-y c2)
+      ))
+     )))
+(define (tangentes-exteriores? c c2)
+  (=
+   (distancia c c2)
+   (+ (Circunferencia-radio c ) (Circunferencia-radio c2))
+   ))
+  
 ;Ejercicio 3
-
+(difine
+ )
