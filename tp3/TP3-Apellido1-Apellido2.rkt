69107aad742185c35ca022c5d83e70b1ee34b4e5
Gaston <gastonmartinezcasto@gmail.com>
2020-05-22 15:11:48 -0300

    Agrego tp3

---
+++ tp3/TP3-Apellido1-Apellido2.rkt
@@ -0,0 +1,19 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname TP2-Apellido1-Apellido2) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#|
+Trabajo Práctico 3: Estructuras
+
+Integrantes:
+- [Apellido, Nombre], comisión [número_comisión].
+- [Apellido, Nombre], comisión [número_comisión].
+|#
+
+;Ejercicio 1
+
+
+;Ejercicio 2
+
+
+;Ejercicio 3
+
