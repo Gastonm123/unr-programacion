7a4ac315777e335ee62088b423b3c98f73480f3d
Gaston <gastonmartinezcasto@gmail.com>
2020-05-22 15:25:37 -0300

    Ejercicio 1

--- tp3/TP3-Apellido1-Apellido2.rkt
+++ tp3/TP3-Apellido1-Apellido2.rkt
@@ -1,6 +1,4 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname TP2-Apellido1-Apellido2) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#lang racket
 #|
 Trabajo Práctico 3: Estructuras
 
@@ -10,7 +8,7 @@ Integrantes:
 |#
 
 ;Ejercicio 1
-
+(struct Circunferencia (x y radio))
 
 ;Ejercicio 2
 
