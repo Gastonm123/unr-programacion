7fed82a93f1a82926a700bc5d19a0757d02e6c05
Gaston <gastonmartinezcastro@gmail.com>
2020-06-16 17:42:48 -0300

    Terminando ejercicio 3 y a medias con el 4

--- TP4-archivos/TP4-Apellido1-Apellido2.rkt
+++ TP4-archivos/TP4-Apellido1-Apellido2.rkt
@@ -91,39 +91,106 @@ Integrantes:
 (define NOTIFICACIONES-HOY (filter dia=dia LISTA-NOTIF))
 (define CONSULTA1 (filter casos>=limite NOTIFICACIONES-HOY))|#
 (define
-  (predicado-punsulta1 Notificacion)
+  (predicado-consulta1 Notificacion)
   (and(string=? HOY (notificacion-fecha Notificacion))
       (>= (string->number(notificacion-conf Notificacion)) LIMITE-CASOS))
  )
 
 (define
   (localidades-limite-casos notificaciones)
-  (map notificacion-loc (filter predicado-punsulta1 notificaciones))
+  (map notificacion-loc (filter predicado-consulta1 notificaciones))
  )
+
 ; [Completar, ejercicio 2-2]
 (define LOCALIDADES-LIMITE-CASOS (localidades-limite-casos  LISTA-NOTIF))
 ;;;;;;;;;;;; Consulta 2
 
 ; [Completar, ejercicio 3-1]
 (define 
-  (listar-departamentos lista)
+  (borrar-repeticiones lista)
   (cond [(empty? lista) empty]
-        [else (cons (first lista) (f (remove-all (first lista) lista)))]
+        [else (cons (first lista) (borrar-repeticiones (remove-all (first lista) lista)))]
    )
   )
 (define
-  (listar-departamentos-repetidos lista)
+  (listar-departamentos lista)
   (cond [(empty? lista) empty]
-        [else (cons (first(first lista)) (g (rest lista)))]
+        [else (cons (first(first lista)) (listar-departamentos (rest lista)))]
    )
  )
 
-(define LISTA-DPTO (listar-departamentos (listar-departamentos-repetidos LISTA-DPTO-LOC)))
+(define LISTA-DPTO (borrar-repeticiones (listar-departamentos LISTA-DPTO-LOC)))
+
 ; [Completar, ejercicio 3-2]
+; Funcion listar-localidades-dpto
+; Dado un departamento y una lista de departamentos y localidades te devuelve una
+; lista de las localidades de ese departamento
+(check-expect
+ (listar-localidades-dpto "9 de Julio" LISTA-DPTO-LOC)
+ (list "ESTEBAN RAMS" "GATO COLORADO" "GREGORIA PEREZ DE DENIS" "LOGROÑO"
+       "MONTEFIORE" "POZO BORRADO" "SANTA MARGARITA" "TOSTADO" "VILLA MINETTI"))
+(check-expect
+ (listar-localidades-dpto "Belgrano" LISTA-DPTO-LOC)
+ (list "ARMSTRONG" "BOUQUET" "LAS PAREJAS" "LAS ROSAS" "MONTES DE OCA" "TORTUGAS"))
+
+(define
+  (listar-localidades-dpto dpto lista)
+  (cond [(empty? lista) empty]
+        [else (if
+               (string=? dpto (first(first lista)))
+               (cons (second(first lista)) (listar-localidades-dpto dpto (rest lista)))
+               (listar-localidades-dpto dpto (rest lista)))])
+  )
+
+; Funcion confirmados-dpto-fecha
+; Dados una lista de notificaciones, un dpto y una fecha devuelve el total de casos confirmados
+; en el dpto a la fecha. Las localidades del departamento que se cuentan son las de LISTA-DPTO-LOC
+(check-expect (confirmados-dpto-fecha "Belgrano" HOY LISTA-NOTIF) 5)
+(check-expect (confirmados-dpto-fecha "9 de Julio" HOY LISTA-NOTIF) 0)
+
+(define
+  (confirmados-dpto-fecha dpto fecha lista)
+  (cond [(empty? lista) 0]
+        [else (if
+               ; condicion
+               (and
+                (string=? (notificacion-fecha (first lista)) fecha)
+                (member (notificacion-loc (first lista))
+                        (listar-localidades-dpto dpto LISTA-DPTO-LOC)))
+               ; si
+               (+ (string->number (notificacion-conf (first lista))) (confirmados-dpto-fecha dpto fecha (rest lista)))
+               ; sino
+               (confirmados-dpto-fecha dpto fecha (rest lista)))]))
 
 ; [Completar, ejercicio 3-3]
+; Funcion confirmados-por-dpto
+; Dada una lista de notificaciones y una fecha devuelve una lista de listas de longitud
+; dos conteniendo, en primera posicion un dpto. y en segundo lugar el numero de casos para
+; ese departamento a la fecha. La lista contiene todos los departamentos de LISTA-DPTO
+; Utilizamos una función auxiliar listar-confirmados-dpto que recursiona sobre la lista
+; de departamentos para devolver el mismo que resultado que confirmados-por-dpto
+(check-expect (confirmados-por-dpto HOY LISTA-NOTIF)
+              (list
+               (list "9 de Julio" 0) (list "Belgrano" 5) (list "Caseros" 3) (list "Castellanos" 22)
+               (list "Constitución" 9) (list "Garay" 3) (list "General López" 18) (list "General Obligado" 4)
+               (list "Iriondo" 7) (list "La Capital" 37) (list "Las Colonias" 4) (list "Rosario" 124)
+               (list "San Cristóbal" 0) (list "San Javier" 1) (list "San Jerónimo" 10) (list "San Justo" 0)
+               (list "San Lorenzo" 17) (list "San Martín" 0) (list "Vera" 1)))
+
+(define
+  (listar-confirmados-dpto fecha lista-notif lista-dpto)
+  (cond [(empty? lista-dpto) empty]
+        [else (cons (list (first lista-dpto)
+                          (confirmados-dpto-fecha (first lista-dpto) fecha lista-notif))
+                    (listar-confirmados-dpto fecha lista-notif (rest lista-dpto)))]))
+
+(define
+  (confirmados-por-dpto fecha lista)
+  (listar-confirmados-dpto fecha lista LISTA-DPTO))
 
 ; [Completar, ejercicio 3-4]
+(define CONFIRMADOS-DPTO-ANTES (confirmados-por-dpto ANTES LISTA-NOTIF))
+(define CONFIRMADOS-DPTO-HOY (confirmados-por-dpto HOY LISTA-NOTIF))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -133,7 +200,42 @@ Integrantes:
 ;;;;;;;;;;;; Consulta 1
 
 ; [Completar, ejercicio 4 - loc-lim-casos.csv]
+; Funcion list-to-string
+; table-to-string: list list a' -> string
+; list a' -> string LOCALIDADES-LIMITE-CASOS
+; first string
+; Esta función recibe una lista de filas y devuelve un string formateado de la lista.
+; Las filas pueden ser listas o strings, lo cual depende de los datos de entrada. En
+; el caso de que una fila sea una lista, los elementos de la lista se separan con ","
+; actual = first lista
+; if (string? actual)
+; then actual + "\n" + (table-to-string (rest lista))
+; else (fila2string actual) + "\n" + (tabla2string (rest lista))
+
+; g : a' String -> String
+(define
+  (g a b)
+  (cond [(string? a) (string-append "\"" a "\"" "," b)]
+        [(number? a) (string-append "\"" (number->string a) "\"" "," b)]
+        [else ""]))
+
+(define
+  (fila2string fila)
+  (foldr g "" fila))
+
+(define
+  (f a b)
+  (cond [(string? a) (string-append "\"" a "\"" "\n" b)]
+        [(list? a) (string-append (fila2string a) "\n" b)]
+        [else ""]))
+
+(define
+  (tabla2string lista)
+  (foldr f "" lista))
+
+(write-file "loc-lim-casos.csv" (tabla2string LOCALIDADES-LIMITE-CASOS))
 
 ;;;;;;; Consulta 2
 
 ; [Completar, ejercicio 4 - casos-por-dpto-hoy.csv y casos-por-dpto-antes.csv]
+(write-file "casos-por-dpto-hoy.csv" (tabla2string CONFIRMADOS-DPTO-HOY))
